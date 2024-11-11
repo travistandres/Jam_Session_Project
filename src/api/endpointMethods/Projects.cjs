647f0ee33d6a821b63ff35d8d8e870889489d97e
Dennon Wilson <todian24@gmail.com>
2024-11-10 20:40:55 -0600

    test this

--- src/api/endpointMethods/Projects.cjs
+++ src/api/endpointMethods/Projects.cjs
@@ -8,7 +8,7 @@ const PORT = 3000
  * @param {String} edited the current time at which the update is taking place
  * @returns {Promise<JSON>} message
  */
-const updateProject = (token, id, name, edited) => {
+export const updateProject = (token, id, name, edited) => {
     return new Promise((res, rej) => {
         setTimeout(() => {
             let json = {}
@@ -19,22 +19,22 @@ const updateProject = (token, id, name, edited) => {
                 if (edited != null){
                     json.edited = edited
                 }
-                const response = fetch(`http://localhost:${PORT}/api/projects/${id}`, {
+                fetch(`http://localhost:${PORT}/api/projects/${id}`, {
                     method: 'PUT',
                     headers: {
                         'Authorization': `Bearer ${token}`,
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(json)
-                });
-        
-                if (!response.ok) {
-                    rej(`HTTP error! Status: ${response.status}`)
-                } else {
-                    const data = response.json();
-                    console.log('Response received:', data);
-                    res(data)
-                }
+                }).then(response => {
+                    if (!response.ok) {
+                        rej(`HTTP error! Status: ${response.status}`)
+                    } else {
+                        const data = response.json();
+                        console.log('Response received:', data);
+                        res(data)
+                    }
+                })
             } catch (error) {
                 rej('Error making the PUT request:', error.message);
             }
@@ -58,22 +58,23 @@ function create(token, name, created){
                     created: created,
                     edited: created
                 }
-                const response = fetch(`http://localhost:${PORT}/api/projects`, {
+                fetch(`http://localhost:${PORT}/api/projects`, {
                     method: 'POST',
                     headers: {
                         'Authorization': `Bearer ${token}`,
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(json)
-                });
+                }).then(response => {
+                    if (!response.ok) {
+                        rej(`HTTP error! Status: ${response.status}`)
+                    } else {
+                        const data = response.json();
+                        console.log('Response received:', data);
+                        res(data)
+                    }
+                })
         
-                if (!response.ok) {
-                    rej(`HTTP error! Status: ${response.status}`)
-                } else {
-                    const data = response.json();
-                    console.log('Response received:', data);
-                    res(data)
-                }
             } catch (error) {
                 rej('Error making the POST request:', error.message);
             }
@@ -87,7 +88,7 @@ function create(token, name, created){
  * @param {String} name project's name
  * @param {String} created date created
  */
-const createProject = (token, name, created) => {
+export const createProject = (token, name, created) => {
     create(token, name, created).then(project => {
         return new Promise((res, rej) => {
             setTimeout(() => {
@@ -95,22 +96,23 @@ const createProject = (token, name, created) => {
                     const json = {
                         projectID: project.projectID
                     }
-                    const response = fetch(`http://localhost:${PORT}/api/userProjects`, {
+                    fetch(`http://localhost:${PORT}/api/userProjects`, {
                         method: 'POST',
                         headers: {
                             'Authorization': `Bearer ${token}`,
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify(json)
-                    });
+                    }).then(response => {
+                        if (!response.ok) {
+                            rej(`HTTP error! Status: ${response.status}`)
+                        } else {
+                            const data = response.json();
+                            console.log('Response received:', data);
+                            res(data)
+                        }
+                    })
             
-                    if (!response.ok) {
-                        rej(`HTTP error! Status: ${response.status}`)
-                    } else {
-                        const data = response.json();
-                        console.log('Response received:', data);
-                        res(data)
-                    }
                 } catch (error) {
                     rej('Error making the POST request:', error.message);
                 }
@@ -125,24 +127,25 @@ const createProject = (token, name, created) => {
  * @param {Int} projectID id of the project to be deleted
  * @returns {Promise<JSON>} message
  */
-const deleteProject = (token, projectID) => {
+export const deleteProject = (token, projectID) => {
     return new Promise((res, rej) => {
         setTimeout(() => {
             try {
-                const response = fetch(`http://localhost:${PORT}/api/projects/${projectID}`, {
+                fetch(`http://localhost:${PORT}/api/projects/${projectID}`, {
                     method: 'DELETE',
                     headers: {
                         'Authorization': `Bearer ${token}`
                     }
-                });
+                }).then(response => {                  
+                    if (!response.ok) {
+                        rej(`HTTP error! Status: ${response.status}`)
+                    } else {
+                        const data = response.json();
+                        console.log('Response received:', data);
+                        res(data)
+                    }
+                })
         
-                if (!response.ok) {
-                    rej(`HTTP error! Status: ${response.status}`)
-                } else {
-                    const data = response.json();
-                    console.log('Response received:', data);
-                    res(data)
-                }
             } catch (error) {
                 rej('Error making the DELETE request:', error.message);
             }
@@ -155,7 +158,7 @@ const deleteProject = (token, projectID) => {
  * @param {String} token the jwt
  * @returns {Promise<JSON>} retrieved rows as a JSON
  */
-const getProjects = (token) => {
+export const getProjects = (token) => {
     return new Promise((res, rej) => {
         setTimeout(() => {
             try {
@@ -178,11 +181,4 @@ const getProjects = (token) => {
             }
         }, 5000)
     })
-}
-
-module.exports = {
-    updateProject,
-    createProject,
-    deleteProject,
-    getProjects
 }
