6c96b30115f6a477c12e49f28d22b640d27d58f2
Dennon Wilson <todian24@gmail.com>
2024-11-10 20:06:02 -0600

    added export to methods.cjs

--- src/api/Methods.cjs
+++ src/api/Methods.cjs
@@ -3,3 +3,25 @@ const {createTextFile, deleteTextFile, updateTextFile, getTextFiles} = require('
 const {createProject, updateProject, deleteProject, getProjects} = require('../api/endpointMethods/Projects.cjs')
 const {createRelation, deleteRelation, getRelations} = require('../api/endpointMethods/UserProjects.cjs')
 const {login, register, updateUser, deleteAccount} = require('../api/endpointMethods/Users.cjs')
+
+module.exports = {
+    createAudioFile,
+    deleteAudioFile,
+    updateAudioFile,
+    getAudioFiles,
+    createTextFile,
+    deleteTextFile,
+    updateTextFile,
+    getTextFiles,
+    createProject,
+    updateProject,
+    deleteProject,
+    getProjects,
+    createRelation,
+    deleteRelation,
+    getRelations,
+    login,
+    register,
+    updateUser,
+    deleteAccount
+}
