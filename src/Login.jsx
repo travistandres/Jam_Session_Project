55efac31416f63dd36809160ff8cba729beec140
travistandres <79394353+travistandres@users.noreply.github.com>
2024-11-17 13:29:00 -0600

    TT 11/17/24 updated login

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import "./index.css";
 import { useNavigate } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { login, register } from "./api/endpointMethods/Users.cjs";
 
 function Login() {
-  const [email, setEmail] = useState("");
+  const [userLogin, setUserLogin] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -16,47 +17,28 @@ function Login() {
     setPasswordVisible(!passwordVisible);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
     setError("");
 
-    const payload = {
-      name: email,
-      password: password,
-    };
+    login(userLogin, password)
+      .then((result) => {
+        console.log("Response data", result);
 
-    try {
-      const response = await fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
+        // decode JWT token to get userData
+        const decoded = jwtDecode(result.token);
+
+        localStorage.setItem("token", result.token);
+        localStorage.setItem("userData", JSON.stringify(decoded));
+        navigate("/home");
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError("Invalid credentials, please try again.");
+        console.error("Error during API call: ", err);
+        setLoading(false);
       });
-
-      if (!response.ok) {
-        throw new Error("Invalid Credentials");
-      }
-
-      const data = await response.json();
-      console.log("Response data", data);
-
-      // decode JWT token to get userData
-      const decoded = jwtDecode(data.token);
-
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("userData", JSON.stringify(decoded));
-      navigate("/home");
-    } catch (error) {
-      console.error("Error during API call: ", error);
-      setError("Invalid credentials, please try again.");
-    } finally {
-      setLoading(false);
-    }
-
-    console.log("Email:", email);
-    console.log("Password:", password);
   };
 
   return (
@@ -70,19 +52,25 @@ function Login() {
             <div>
               <h1 className="text-5xl text-center">Welcome!</h1>
             </div>
-            <form onSubmit={handleSubmit} className="mt-6 max-w-xs mx-auto">
-              <div className="password-unmet h-7">
+            <form
+              onSubmit={(e) => {
+                e.stopPropagation(); // Prevent event from bubbling up
+                handleSubmit(e);
+              }}
+              className="mt-6 max-w-xs mx-auto"
+            >
+              <div className="password-unmet h-7 text-xs">
                 {error && <p>{error}</p>}
               </div>
               <div className="mb-4">
                 <input
                   type="text"
                   id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  value={userLogin}
+                  onChange={(e) => setUserLogin(e.target.value)}
                   className="border p-2 w-full rounded-lg text-black textfield-bg"
                   required
-                  placeholder="Email"
+                  placeholder="Email or Username"
                 />
               </div>
               <div className="mb-5 relative">
